5109832311fe21d5bdbe8ce86898f7bfa39eb194
pollenjp <polleninjp@gmail.com>
2017-12-13 06:03:53 +0900

    [script.js] delete line end spaces

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 var scene = new THREE.Scene();
 var renderer = new THREE.WebGLRenderer({
     antialias: true,
-    alpha: true,                                         
+    alpha: true,
 });
-renderer.setClearColor(new THREE.Color("black"), 0);   
-renderer.setSize(640, 480);                            
-renderer.domElement.style.position = "absolute";       
-renderer.domElement.style.top = "0px";                 
-renderer.domElement.style.left = "0px";                
-document.body.appendChild(renderer.domElement);        
-var camera = new THREE.Camera();                       
-scene.add(camera);                                     
-var light = new THREE.DirectionalLight(0xffffff);      
-light.position.set(0, 0, 2);                           
-scene.add(light);                                      
+renderer.setClearColor(new THREE.Color("black"), 0);
+renderer.setSize(640, 480);
+renderer.domElement.style.position = "absolute";
+renderer.domElement.style.top = "0px";
+renderer.domElement.style.left = "0px";
+document.body.appendChild(renderer.domElement);
+var camera = new THREE.Camera();
+scene.add(camera);
+var light = new THREE.DirectionalLight(0xffffff);
+light.position.set(0, 0, 2);
+scene.add(light);
 
 //var controls = new THREE.OrbitControls( camera, renderer.domElement );
 
 
 
-var source = new THREEx.ArToolkitSource({              
-    sourceType: "webcam",                                
+var source = new THREEx.ArToolkitSource({
+    sourceType: "webcam",
 });
-source.init(function onReady() {                       
-    onResize();                                          
+source.init(function onReady() {
+    onResize();
+});
+
+
+
+var context = new THREEx.ArToolkitContext({
+    debug: false,
+    cameraParametersUrl: "./data/camera_para.dat",
+    detectionMode: "mono",
+    imageSmoothingEnabled: true,
+    maxDetectionRate: 60,
+    canvasWidth: source.parameters.sourceWidth,
+    canvasHeight: source.parameters.sourceHeight,
+});
+context.init(function onCompleted(){
+    camera.projectionMatrix.copy(context.getProjectionMatrix());
 });
 
 
 
-var context = new THREEx.ArToolkitContext({            
-    debug: false,                                        
-    cameraParametersUrl: "./data/camera_para.dat",              
-    detectionMode: "mono",                               
-    imageSmoothingEnabled: true,                         
-    maxDetectionRate: 60,                                
-    canvasWidth: source.parameters.sourceWidth,          
-    canvasHeight: source.parameters.sourceHeight,        
-});
-context.init(function onCompleted(){                   
-    camera.projectionMatrix.copy(context.getProjectionMatrix());    
+
+window.addEventListener("resize", function() {
+    onResize();
 });
 
- 
- 
- 
-window.addEventListener("resize", function() {         
-    onResize();                                          
-});
- 
 function onResize(){
     source.onResizeElement();
-    source.copyElementSizeTo(renderer.domElement);       
-    if(context.arController !== null){                   
-        source.copyElementSizeTo(context.arController.canvas);   
-    } 
+    source.copyElementSizeTo(renderer.domElement);
+    if(context.arController !== null){
+        source.copyElementSizeTo(context.arController.canvas);
+    }
 }
 
 
 ////////////////////////////////////////////////////////////////////////////
 //////////hiro-marker
-var hiro_marker = new THREE.Group();                       
+var hiro_marker = new THREE.Group();
 var controls = new THREEx.ArMarkerControls(context, hiro_marker, {
-    type: "pattern",                                     
-    patternUrl: "./patt/hiro.patt",                             
+    type: "pattern",
+    patternUrl: "./patt/hiro.patt",
 });
 scene.add(hiro_marker);
 
@@ -79,10 +79,10 @@ loader.load("./gltf/face/face.gltf", function( gltf ){
 
 //////////////////////////////////////////////////////////////////////////////
 ////////////kanji_marker
-var kanji_marker = new THREE.Group();                       
+var kanji_marker = new THREE.Group();
 var controls = new THREEx.ArMarkerControls(context, kanji_marker, {
-    type: "pattern",                                     
-    patternUrl: "./patt/kanji.patt",                             
+    type: "pattern",
+    patternUrl: "./patt/kanji.patt",
 });
 scene.add(kanji_marker);
 
@@ -98,10 +98,10 @@ loader.load("./gltf/monkey/monkey.gltf", function( gltf ){
 
 //////////////////////////////////////////////////////////////////////////////
 ////////////utv-marker
-var utv_marker = new THREE.Group();                       
+var utv_marker = new THREE.Group();
 var controls = new THREEx.ArMarkerControls(context, utv_marker, {
-    type: "pattern",                                     
-    patternUrl: "./patt/ut-virtual.patt",                             
+    type: "pattern",
+    patternUrl: "./patt/ut-virtual.patt",
 });
 scene.add(utv_marker);
 
@@ -121,10 +121,10 @@ var gltf = null;
 var mixer = null;
 var clock = new THREE.Clock();
 
-var utv_tamago_ware = new THREE.Group();                       
+var utv_tamago_ware = new THREE.Group();
 var controls = new THREEx.ArMarkerControls(context, utv_tamago_ware, {
-    type: "pattern",                                     
-    patternUrl: "./patt/utvirtual_tamagohibiware_pattern_marker.patt",                             
+    type: "pattern",
+    patternUrl: "./patt/utvirtual_tamagohibiware_pattern_marker.patt",
 });
 scene.add(utv_tamago_ware);
 
@@ -158,14 +158,14 @@ animate();
 //////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
-function renderScene() {                               
-    requestAnimationFrame(renderScene);                  
-    if(source.ready === false)    { return; }              
-    context.update(source.domElement);                   
-    renderer.render(scene, camera);                      
+function renderScene() {
+    requestAnimationFrame(renderScene);
+    if(source.ready === false)    { return; }
+    context.update(source.domElement);
+    renderer.render(scene, camera);
 }
 
 //////////////////////////////////////////////////////////////////////////////
 //var orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
-renderScene();                                         
+renderScene();
 
